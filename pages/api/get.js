de44150cba7a2b907bce0ba16008d9eb1cc6c2b8
TH2FEAR <80583960+TH2FEAR@users.noreply.github.com>
2025-05-20 16:39:11 +0300

    Update get.js

--- pages/api/get.js
+++ pages/api/get.js
@@ -1,5 +1,3 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
   const { url } = req.query;
   if (!url) return res.status(400).json({ error: "url eksik kanka" });
